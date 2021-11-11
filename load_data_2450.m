ec89a49996ff01b148e14dee1a8a560dac75e11a
emanuele.buchicchio <emanuele.buchicchio@smartpeg.eu>
2021-11-11 12:06:20 +0100

    ref: cleanup comments

--- load_data_2450.m
+++ load_data_2450.m
@@ -1,22 +1,23 @@
 close all
 clear
 clc
-
-% Fs = 200; % sampling rate
-% Fs = 6.308835234897145e+02
-
+% frequency sweep
 f0_vector = [0.05 0.1 0.2 0.4 1 2 4 10 20 40];
+data_file_prefix= './data/test_battery_readback_5ms_50ma_autoOFF_nplc_01_';
+
 
 for idx_f0 = 1 : length(f0_vector)
     
     f0 = f0_vector(idx_f0)
-    
-    fid = fopen(['test_battery_readback_5ms_50ma_autoOFF_nplc_01_' ...
-        num2str(f0) '.csv']);
+    %
+    % Foreach frequency  
+    % Read current and voltage data from file
+    %
+    fid = fopen([ data_file_prefix num2str(f0) '.csv']);
     line=fgetl(fid); % discard header
 
     count = 1;
-%     tic
+    %     tic
     while (1)
         line = fgetl(fid);
         if line == -1
@@ -33,12 +34,6 @@ for idx_f0 = 1 : length(f0_vector)
     
     t = (t-t(1))*3600*24; % convert to seconds
     
-%     %%%%%% resample
-%     [sig_i_resampled,t_resampled] = resample(sig_i,t);
-%     sig_v_resampled = resample(sig_v,t);
-%     sig_i = sig_i_resampled;
-%     sig_v = sig_v_resampled;
-%     t = t_resampled;
 
     Fs = 1/mean(diff(t))
     
@@ -70,8 +65,6 @@ for idx_f0 = 1 : length(f0_vector)
     f_base = 0 : Fs/N : Fs - Fs/N;
     V = fft(sig_v(:)-mean(sig_v))/N; 
     I = fft(sig_i(:)-mean(sig_i))/N;
-%     V = fft(sig_v(:).*hanning(N))/N; % windowing
-%     I = fft(sig_i(:).*hanning(N))/N;
 
     figure(3);
     semilogx(f_base, db(V), 'b.')
@@ -87,22 +80,16 @@ for idx_f0 = 1 : length(f0_vector)
     drawnow
     
     
-%     idx_freq = round(N*f0/Fs+1); % index of the bin of interest
     idx_freq_v = find(abs(V(1:N/2))==max(abs(V(1:N/2))));
     idx_freq_i = find(abs(I(1:N/2))==max(abs(I(1:N/2))));
         
     f_peak_v = f_base(idx_freq_v) % DEBUG
     f_peak_i = f_base(idx_freq_i) % DEBUG    
         
-%     Z_vector(idx_f0) = V(idx_freq)./I(idx_freq);
     Z_vector(idx_f0) = V(idx_freq_v)./I(idx_freq_i);
     
     Z_abs = db(Z_vector(idx_f0))    
-    
-%     sig_i_matrix(:,idx_f0) = sig_i(:);
-%     sig_v_matrix(:,idx_f0) = sig_v(:);
-%     t_matrix(:,idx_f0) = t(:);
-    
+        
 end
 
 
@@ -117,19 +104,14 @@ grid on
 subplot(212)
 semilogx(f0_vector, rad2deg(angle((Z_vector))),'-b.');
 hold on;
-% semilogx(f0_vector, ... % delay compensation
-%     rad2deg(angle((Z_vector.*exp(-1j*2*pi*f0_vector*(3e-4))))),'r');
 xlabel('frequency [Hz]')
 ylabel('phase [deg]')
 grid on
 
 figure;
 plot(real(Z_vector), -imag(Z_vector))
-% axis equal
 xlabel('Re(Z) [\Omega]')
 ylabel('-Im(Z) [\Omega]')
 grid on
 hold on;
-% text(real(Z_vector(1)), -imag(Z_vector(1)),num2str())
-
 
